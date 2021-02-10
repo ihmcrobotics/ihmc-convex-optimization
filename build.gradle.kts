4967037f1ecf2b5f480390fda514b89340066c9e
Robert <rgriffin@ihmc.us>
2021-02-10 09:51:38 -0600

    :bookmark: 0.17.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.17.3"
+   version = "0.17.4"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-convex-optimization"
    openSource = true
 
