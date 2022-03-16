ee671d0d1ad230eadd890d9a6cc4086dd514d077
smccrory <smccrory@ihmc.us>
2022-03-16 10:18:13 -0500

    Including test with equality constraints

--- src/test/java/us/ihmc/convexOptimization/linearProgram/LinearProgramSolverTest.java
+++ src/test/java/us/ihmc/convexOptimization/linearProgram/LinearProgramSolverTest.java
@@ -8,8 +8,10 @@ import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tools.EuclidCoreTools;
+import us.ihmc.matrixlib.MatrixTools;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,22 +32,72 @@ public class LinearProgramSolverTest
 
       for (int i = 0; i < tests; i++)
       {
-         Pair<DMatrixRMaj, DMatrixRMaj> constraintPlanes = generatePlanesTangentToRandomEllipsoid();
+         Pair<DMatrixRMaj, DMatrixRMaj> constraintSet = generateRandomEllipsoidBasedConstraintSet(false);
+         DMatrixRMaj A = constraintSet.getLeft();
+         DMatrixRMaj b = constraintSet.getRight();
 
          for (int j = 0; j < costVectorsPerProblem; j++)
          {
-            DMatrixRMaj costVector = generateRandomCostVector(constraintPlanes.getLeft().getNumCols());
+            DMatrixRMaj costVector = generateRandomCostVector(A.getNumCols());
 
             // SOLVE WITH APACHE //
-            double[] apacheCommonsSolution = solveWithApacheCommons(constraintPlanes.getLeft(), constraintPlanes.getRight(), costVector, Relationship.LEQ);
+            double[] apacheCommonsSolution = solveWithApacheCommons(A, b, costVector, Relationship.LEQ);
 
             // SOLVER WITH CUSTOM IMPL USING SIMPLEX //
             DMatrixRMaj simplexSolution = new DMatrixRMaj(0);
-            boolean foundSimplexSolution = customSolver.solve(costVector, constraintPlanes.getLeft(), constraintPlanes.getRight(), simplexSolution, SolverMethod.SIMPLEX);
+            boolean foundSimplexSolution = customSolver.solve(costVector, A, b, simplexSolution, SolverMethod.SIMPLEX);
 
             // SOLVER WITH CUSTOM IMPL USING CRISS CROSS //
             DMatrixRMaj crissCrossSolution = new DMatrixRMaj(0);
-            boolean foundCrissCrossSolution = customSolver.solve(costVector, constraintPlanes.getLeft(), constraintPlanes.getRight(), crissCrossSolution, SolverMethod.CRISS_CROSS);
+            boolean foundCrissCrossSolution = customSolver.solve(costVector, A, b, crissCrossSolution, SolverMethod.CRISS_CROSS);
+
+            if (apacheCommonsSolution == null)
+            {
+               Assertions.assertFalse(foundSimplexSolution);
+               Assertions.assertFalse(foundCrissCrossSolution);
+            }
+            else
+            {
+               Assertions.assertTrue(foundSimplexSolution);
+               Assertions.assertTrue(foundCrissCrossSolution);
+
+               for (int k = 0; k < apacheCommonsSolution.length; k++)
+               {
+                  Assertions.assertTrue(EuclidCoreTools.epsilonEquals(apacheCommonsSolution[k], simplexSolution.get(k), epsilon));
+                  Assertions.assertTrue(EuclidCoreTools.epsilonEquals(apacheCommonsSolution[k], crissCrossSolution.get(k), epsilon));
+               }
+            }
+         }
+      }
+   }
+
+   @Test
+   public void testEllipsoidBasedMaxAndEqualityConstraints()
+   {
+      int tests = 400;
+      int costVectorsPerProblem = 10;
+      LinearProgramSolver customSolver = new LinearProgramSolver();
+
+      for (int i = 0; i < tests; i++)
+      {
+         Pair<DMatrixRMaj, DMatrixRMaj> constraintPlanes = generateRandomEllipsoidBasedConstraintSet(true);
+         DMatrixRMaj A = constraintPlanes.getLeft();
+         DMatrixRMaj b = constraintPlanes.getRight();
+
+         for (int j = 0; j < costVectorsPerProblem; j++)
+         {
+            DMatrixRMaj costVector = generateRandomCostVector(A.getNumCols());
+
+            // SOLVE WITH APACHE //
+            double[] apacheCommonsSolution = solveWithApacheCommons(A, b, costVector, Relationship.LEQ);
+
+            // SOLVER WITH CUSTOM IMPL USING SIMPLEX //
+            DMatrixRMaj simplexSolution = new DMatrixRMaj(0);
+            boolean foundSimplexSolution = customSolver.solve(costVector, A, b, simplexSolution, SolverMethod.SIMPLEX);
+
+            // SOLVER WITH CUSTOM IMPL USING CRISS CROSS //
+            DMatrixRMaj crissCrossSolution = new DMatrixRMaj(0);
+            boolean foundCrissCrossSolution = customSolver.solve(costVector, A, b, crissCrossSolution, SolverMethod.CRISS_CROSS);
 
             if (apacheCommonsSolution == null)
             {
@@ -76,24 +128,26 @@ public class LinearProgramSolverTest
 
       for (int i = 0; i < tests; i++)
       {
-         Pair<DMatrixRMaj, DMatrixRMaj> constraintPlanes = generatePlanesTangentToRandomEllipsoid();
-         CommonOps_DDRM.scale(-1.0, constraintPlanes.getLeft());
-         CommonOps_DDRM.scale(-1.0, constraintPlanes.getRight());
+         Pair<DMatrixRMaj, DMatrixRMaj> constraintPlanes = generateRandomEllipsoidBasedConstraintSet(false);
+         DMatrixRMaj A = constraintPlanes.getLeft();
+         DMatrixRMaj b = constraintPlanes.getRight();
+         CommonOps_DDRM.scale(-1.0, A);
+         CommonOps_DDRM.scale(-1.0, b);
 
          for (int j = 0; j < costVectorsPerProblem; j++)
          {
-            DMatrixRMaj costVector = generateRandomCostVector(constraintPlanes.getLeft().getNumCols());
+            DMatrixRMaj costVector = generateRandomCostVector(A.getNumCols());
 
             // SOLVE WITH APACHE //
-            double[] apacheCommonsSolution = solveWithApacheCommons(constraintPlanes.getLeft(), constraintPlanes.getRight(), costVector, Relationship.LEQ);
+            double[] apacheCommonsSolution = solveWithApacheCommons(A, b, costVector, Relationship.LEQ);
 
             // SOLVER WITH CUSTOM IMPL USING SIMPLEX //
             DMatrixRMaj simplexSolution = new DMatrixRMaj(0);
-            boolean foundSimplexSolution = customSolver.solve(costVector, constraintPlanes.getLeft(), constraintPlanes.getRight(), simplexSolution, SolverMethod.SIMPLEX);
+            boolean foundSimplexSolution = customSolver.solve(costVector, A, b, simplexSolution, SolverMethod.SIMPLEX);
 
             // SOLVER WITH CUSTOM IMPL USING CRISS CROSS //
             DMatrixRMaj crissCrossSolution = new DMatrixRMaj(0);
-            boolean foundCrissCrossSolution = customSolver.solve(costVector, constraintPlanes.getLeft(), constraintPlanes.getRight(), crissCrossSolution, SolverMethod.CRISS_CROSS);
+            boolean foundCrissCrossSolution = customSolver.solve(costVector, A, b, crissCrossSolution, SolverMethod.CRISS_CROSS);
 
             if (apacheCommonsSolution == null)
             {
@@ -162,12 +216,12 @@ public class LinearProgramSolverTest
    }
 
    /**
-    * Sets A and b matrices to be planes that are tangent to an ellipsoid
+    * Sets inequality matrices to be planes that are tangent to an ellipsoid. Sets equality matrices to converge at a point interior to the ellipsoid.
     */
-   private static Pair<DMatrixRMaj, DMatrixRMaj> generatePlanesTangentToRandomEllipsoid()
+   private static Pair<DMatrixRMaj, DMatrixRMaj> generateRandomEllipsoidBasedConstraintSet(boolean includeEqualityConstraints)
    {
       int dimensionality = 2 + random.nextInt(30);
-      int constraints = 1 + random.nextInt(30);
+      int inequalityConstraints = 1 + random.nextInt(30);
 
       double radiusSquared = 1.0 + 100.0 * random.nextDouble();
       double[] alphas = new double[dimensionality];
@@ -176,22 +230,13 @@ public class LinearProgramSolverTest
          alphas[j] = 1.0 + 30.0 * random.nextDouble();
       }
 
-      DMatrixRMaj A = new DMatrixRMaj(constraints, dimensionality);
-      DMatrixRMaj b = new DMatrixRMaj(constraints, 1);
+      DMatrixRMaj Ain = new DMatrixRMaj(inequalityConstraints, dimensionality);
+      DMatrixRMaj bin = new DMatrixRMaj(inequalityConstraints, 1);
 
-      for (int i = 0; i < constraints; i++)
+      for (int i = 0; i < inequalityConstraints; i++)
       {
          // compute initial point on curve
-         double[] initialPoint = new double[dimensionality];
-         double remainingPosValue = radiusSquared;
-         for (int k = 0; k < dimensionality - 1; k++)
-         {
-            double alphaXSq = EuclidCoreRandomTools.nextDouble(random, 0.0, remainingPosValue * 0.99 / alphas[k]);
-            remainingPosValue -= alphaXSq;
-            initialPoint[k] = Math.sqrt(alphaXSq / alphas[k]);
-         }
-
-         initialPoint[dimensionality - 1] = Math.sqrt(remainingPosValue / alphas[dimensionality - 1]);
+         double[] initialPoint = generatePointOnEllipsoid(dimensionality, radiusSquared, alphas);
 
          // compute gradient at this point
          double[] gradient = new double[dimensionality];
@@ -208,13 +253,101 @@ public class LinearProgramSolverTest
 
          for (int j = 0; j < dimensionality; j++)
          {
-            A.set(i, j, gradient[j]);
+            Ain.set(i, j, gradient[j]);
          }
 
-         b.set(i, 0, bValue);
+         bin.set(i, 0, bValue);
       }
 
+      if (!includeEqualityConstraints)
+      {
+         return Pair.of(Ain, bin);
+      }
+
+      int equalityConstraints = 1 + random.nextInt(dimensionality - 1);
+      DMatrixRMaj Aeq = new DMatrixRMaj(equalityConstraints, dimensionality);
+      DMatrixRMaj beq = new DMatrixRMaj(equalityConstraints, 1);
+
+      double[] interiorPoint = generatePointOnEllipsoid(dimensionality, radiusSquared, alphas);
+      double scale = 0.9 * random.nextDouble();
+      for (int i = 0; i < interiorPoint.length; i++)
+      {
+         interiorPoint[i] = scale * interiorPoint[i];
+      }
+
+      for (int i = 0; i < equalityConstraints; i++)
+      {
+         double normDotPoint = 0.0;
+         double[] normal = generateRandomVectorForPlaneNormal(dimensionality);
+
+         for (int j = 0; j < dimensionality; j++)
+         {
+            normDotPoint += normal[j] * interiorPoint[j];
+         }
+
+         beq.set(i, 0, normDotPoint);
+
+         for (int j = 0; j < dimensionality; j++)
+         {
+            Aeq.set(i, j, normal[j]);
+         }
+      }
+
+      int constraints = inequalityConstraints + 2 * equalityConstraints;
+      DMatrixRMaj A = new DMatrixRMaj(constraints, dimensionality);
+      DMatrixRMaj b = new DMatrixRMaj(constraints, 1);
+
+      MatrixTools.setMatrixBlock(A, 0, 0, Ain, 0, 0, Ain.getNumRows(), Ain.getNumCols(), 1.0);
+      MatrixTools.setMatrixBlock(A, Ain.getNumRows(), 0, Aeq, 0, 0, Aeq.getNumRows(), Aeq.getNumCols(), 1.0);
+      MatrixTools.setMatrixBlock(A, Ain.getNumRows() + Aeq.getNumRows(), 0, Aeq, 0, 0, Aeq.getNumRows(), Aeq.getNumCols(), -1.0);
+
+      MatrixTools.setMatrixBlock(b, 0, 0, bin, 0, 0, bin.getNumRows(), bin.getNumCols(), 1.0);
+      MatrixTools.setMatrixBlock(b, bin.getNumRows(), 0, beq, 0, 0, beq.getNumRows(), beq.getNumCols(), 1.0);
+      MatrixTools.setMatrixBlock(b, bin.getNumRows() + beq.getNumRows(), 0, beq, 0, 0, beq.getNumRows(), beq.getNumCols(), -1.0);
+
       return Pair.of(A, b);
+   }
+
+   private static double[] generatePointOnEllipsoid(int dimensionality, double radiusSquared, double[] alphas)
+   {
+      double[] initialPoint = new double[dimensionality];
+      double remainingPosValue = radiusSquared;
+      for (int k = 0; k < dimensionality - 1; k++)
+      {
+         double alphaXSq = EuclidCoreRandomTools.nextDouble(random, 0.0, remainingPosValue * 0.99 / alphas[k]);
+         remainingPosValue -= alphaXSq;
+         initialPoint[k] = Math.sqrt(alphaXSq / alphas[k]);
+      }
+
+      initialPoint[dimensionality - 1] = Math.sqrt(remainingPosValue / alphas[dimensionality - 1]);
+      return initialPoint;
+   }
+
+   private static double[] generateRandomVectorForPlaneNormal(int dimensionality)
+   {
+      double[] v = new double[dimensionality];
+      double sumSq = 0.0;
+
+      for (int i = 0; i < dimensionality; i++)
+      {
+         v[i] = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         sumSq += MathTools.square(v[i]);
+      }
+
+      double norm = Math.sqrt(sumSq);
+      if (norm < 1e-3)
+      {
+         v[0] = 1.0;
+      }
+      else
+      {
+         for (int i = 0; i < dimensionality; i++)
+         {
+            v[i] /= Math.sqrt(1.0 / norm);
+         }
+      }
+
+      return v;
    }
 
    /**
