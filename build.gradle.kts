43e6ef9b0a1857924b3cf48feb550a29ac726015
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-07 14:31:56 -0500

    :bookmark: 0.17.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.17.11"
+   version = "0.17.12"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-convex-optimization"
    openSource = true
 
