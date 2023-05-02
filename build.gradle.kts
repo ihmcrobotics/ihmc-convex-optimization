dd180d54f19a95b0ce2fd7e531b2e47f95cb3bda
smccrory <smccrory@ihmc.us>
2023-05-02 13:26:55 -0500

    :bookmark: 0.17.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.17.17"
+   version = "0.17.18"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-convex-optimization"
    openSource = true
 
